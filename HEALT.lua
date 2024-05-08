49ecd724664919a5c29d3245b6a6b2856d4a5a85
LUATT11 <167495857+LUATT11@users.noreply.github.com>
2024-05-07 23:26:11 -0500

    Create HEALT.lua

---
+++ HEALT.lua
@@ -0,0 +1,26 @@
+local player = game:GetService("Players").LocalPlayer
+local playerHumanoid = player.Character and player.Character:FindFirstChildOfClass("Humanoid")
+
+if playerHumanoid then
+    local RunService = game:GetService("RunService")
+    local connection
+    connection = RunService.RenderStepped:Connect(function()
+        playerHumanoid.Health = math.huge
+    end)
+
+    -- Desconectar el evento cuando el personaje del jugador ya no existe
+    player.CharacterAdded:Connect(function(character)
+        playerHumanoid = character:FindFirstChildOfClass("Humanoid")
+        if playerHumanoid then
+            connection = RunService.RenderStepped:Connect(function()
+                playerHumanoid.Health = math.huge
+            end)
+        else
+            connection:Disconnect()
+        end
+    end)
+
+    player.CharacterRemoved:Connect(function()
+        connection:Disconnect()
+    end)
+end
